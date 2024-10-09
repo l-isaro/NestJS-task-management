11386b2766a9f3c5761bf8900a514f21ef8fa7a0
l-isaro <l.isaro@alustudent.com>
2024-10-09 15:01:13 +0200

    feat: add validation for deleting a task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { GetTaskFilterDto } from './dto/get-task-filter';
@@ -11,7 +11,11 @@ export class TasksService {
   }
 
   getTaskById(id): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException();
+    }
+    return found;
   }
 
   createTask(createTaskDto): Task {
@@ -27,7 +31,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
