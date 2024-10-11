f39258f705083f5fc97fcb1be4eed6afd2a83e8e
l-isaro <l.isaro@alustudent.com>
2024-10-11 15:17:23 +0200

    Refactor tasks repository and task entity

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,4 +1,6 @@
 import { Repository } from 'typeorm';
 import { Task } from './entity/task.entity';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class TasksRepository extends Repository<Task> {}
